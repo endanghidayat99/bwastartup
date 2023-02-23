df5182cb22be76d4e5c07a96c57d3164b7eaa392
endang <endang.hidayat@collega.co.id>
2023-02-23 18:42:23 +0700

    init

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+	//
+	//fmt.Println("Connection to Database is good")
+	//
+	//var users []user.User
+	//length := len(users)
+	//fmt.Println(length)
+	//db.Find(&users)
+	//length = len(users)
+	//fmt.Println(length)
+	//
+	//for _, user := range users {
+	//	fmt.Println(user.Name)
+	//	fmt.Println(user.Email)
+	//	fmt.Println("=============")
+	//}
+
+	//router := gin.Default()
+	//router.GET("/handler", handler)
+	//
+	//router.Run()
+}
+
+//
+//func handler(c *gin.Context) {
+//	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+//	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+//
+//	if err != nil {
+//		log.Fatal(err.Error())
+//	}
+//
+//	var users []user.User
+//	db.Find(&users)
+//
+//	c.JSON(http.StatusOK, users)
+//}
---
+++ user/repository.go
@@ -0,0 +1,28 @@
+package user
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+)
+
+type Repository interface {
+	Save(user User) (User, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+
+}
+
+func (r *repository) Save(user User) (User, error) {
+	fmt.Println("Simpan Data")
+	err := r.db.Create(&user).Error
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
