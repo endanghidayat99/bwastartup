8d7c31a2f85b9f729f08129a006b4b3029a8d4ec
endang <endang.hidayat@collega.co.id>
2023-02-24 06:57:25 +0700

    add login service

--- user/repository.go
+++ user/repository.go
@@ -7,6 +7,7 @@ import (
 
 type Repository interface {
 	Save(user User) (User, error)
+	FindByEmail(email string) (User, error)
 }
 
 type repository struct {
@@ -21,6 +22,15 @@ func NewRepository(db *gorm.DB) *repository {
 func (r *repository) Save(user User) (User, error) {
 	fmt.Println("Simpan Data")
 	err := r.db.Create(&user).Error
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (r *repository) FindByEmail(email string) (User, error) {
+	var user User
+	err := r.db.Where("email = ?", email).Find(&user).Error
 	if err != nil {
 		return user, err
 	}
