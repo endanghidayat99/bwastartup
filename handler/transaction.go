54be112c92c1a826afbb891b1202311c7f3cd110
endang <endang.hidayat@collega.co.id>
2023-03-15 19:43:07 +0700

    service transaction -add service get transactions by user

--- handler/transaction.go
+++ handler/transaction.go
@@ -42,3 +42,18 @@ func (h *transactionHandler) GetCampaignTransaction(c *gin.Context) {
 	response := helper.APIResponse("Campaign Transactions", http.StatusOK, "success", transaction.FormatCampaignTransactions(transactions))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *transactionHandler) GetUserTransactions(c *gin.Context) {
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
+
+	transactions, err := h.service.GetTransactionByUserID(userID)
+	if err != nil {
+		response := helper.APIResponse("Failed to get users's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("User's Transactions", http.StatusOK, "success", transaction.FormatUsersTransactions(transactions))
+	c.JSON(http.StatusOK, response)
+}
