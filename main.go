0b6639d6975b76b1c07f5ca1661b432bc9ff31a1
endang <endang.hidayat@collega.co.id>
2023-04-04 13:04:26 +0700

    change db

--- main.go
+++ main.go
@@ -18,8 +18,62 @@ import (
 	"strings"
 )
 
+//func reassignedPriorities(priorities []int32) []int32 {
+//	// Write your code here
+//	hashMap := make(map[int32]int32)
+//	n := len(priorities)
+//	tempArr := make([]int32, n)
+//	for i := 0; i < n; i++ {
+//		tempArr[i] = priorities[i]
+//	}
+//	sort.Slice(tempArr, func(i, j int) bool { return tempArr[i] < tempArr[j] })
+//	var priority int32
+//	priority = 1
+//	for i := 0; i < n; i++ {
+//		if i > 0 {
+//			if tempArr[i-1] != tempArr[i] {
+//				priority += 1
+//			}
+//		}
+//		hashMap[tempArr[i]] = priority
+//	}
+//
+//	for i := 0; i < n; i++ {
+//		priorities[i] = hashMap[priorities[i]]
+//	}
+//
+//	return priorities
+//}
+//
+//func frequencyOfMaxValue(numbers []int32, q []int32) []int32 {
+//	n := len(numbers)
+//	answer := make([]int32, n)
+//	for i := 0; i < n; i++ {
+//		answer[i] = -1
+//	}
+//	maxvalue, count := -1, 1
+//	for i := n - 1; i >= 0; i-- {
+//		if int(numbers[i]) == maxvalue {
+//			count += 1
+//		}
+//		if int(numbers[i]) > maxvalue {
+//			maxvalue = int(numbers[i])
+//			count = 1
+//		}
+//		answer[i] = int32((count))
+//	}
+//	return answer
+//}
+//
+//func main() {
+//	numbers := []int32{2, 1, 2}
+//	q := []int32{1, 2, 3}
+//
+//	fmt.Println(frequencyOfMaxValue(numbers, q))
+//}
+
 func main() {
-	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "sql12610918:CxakkFFBWc@tcp(sql12.freemysqlhosting.net:3306)/sql12610918?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
